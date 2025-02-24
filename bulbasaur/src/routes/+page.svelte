7a9dda88cf4988b5666ff1142f61fb36eec6b501
itsmexxie <wendy.plaskova05@gmail.com>
2025-02-24 23:17:30 +0100

    smaller bulbasaur docker image

---
+++ bulbasaur/src/routes/+page.svelte
@@ -0,0 +1,7 @@
+<div class="container-xl p-3">
+	<p>Klikněte na:</p>
+	<ul>
+        <li><a href="/address">Adresy</a> pro zobrazení informací ohledně konkrétní adresy.</li>
+        <li><a href="/map">Mapa</a> pro zobrazení mapy určité IPv4 sítě.</li>
+    </ul>
+</div>
